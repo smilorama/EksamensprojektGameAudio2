639f9de9190f426b17a98c2a538b2a0087c0873a
smilorama <83668776+smilorama@users.noreply.github.com>
2026-06-11 22:59:21 +0200

    Nye IR i wwise, rettet indstillinger i Wwise og Unity, sat Ambisonics rigtigt op i både wwise og unity
    Nye IR i wwise, rettet indstillinger i Wwise og Unity, sat Ambisonics rigtigt op i både wwise og unity

---
+++ EksamensprojektUnity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_GLASS = 3819089238U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BITCRUSH = 1974573347U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID REVERB_AUDIO_BUS = 2954846929U;
+        static const AkUniqueID REVERB_STEREO = 1715656151U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CHURCH_SMALL_WOOD = 484890516U;
+        static const AkUniqueID PARKERINGREV_001_BAELTE1 = 288340792U;
+        static const AkUniqueID PARKERINGREV_002_BAELTE2 = 1020492676U;
+        static const AkUniqueID PARKERINGREV_003_KLAP1 = 230976631U;
+        static const AkUniqueID PARKERINGREV_004_KLAP2 = 3950004767U;
+        static const AkUniqueID PARKERINGREV_005_KLAP3 = 903063735U;
+        static const AkUniqueID PARKERINGREV_006_KLAP4 = 2665109655U;
+        static const AkUniqueID PARKERINGREV_007_KLAP5 = 2767839671U;
+        static const AkUniqueID PARKERINGREV_008_KLAP6 = 3813691439U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
