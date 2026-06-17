c07aebdf66a6bf56e26bb27f8604c317e408a38e
Nico133a <Nico.rindsig@gmail.com>
2026-06-17 16:31:54 +0200

    Distance to Cave

---
+++ EksamensprojektUnity/Assets/Scripts/DistanceToCaveParameter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DistanceToCaveParameter : MonoBehaviour
+{
+    [SerializeField] private float _maxDistance = 40f;
+
+    private Transform _player;
+
+    private void Start()
+    {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null) _player = playerObj.transform;
+    }
+
+    private void Update()
+    {
+        if (_player == null) return;
+
+        float dist = Vector3.Distance(transform.position, _player.position);
+        float value = (1f - Mathf.Clamp01(dist / _maxDistance)) * 100f;
+        AkUnitySoundEngine.SetRTPCValue("Distance_to_Cave", value);
+    }
+}
